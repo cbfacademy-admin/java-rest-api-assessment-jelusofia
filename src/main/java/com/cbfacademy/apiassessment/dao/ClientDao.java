e4eb976622b28a75e137c94390c89a6535f98525
Jelusara Fernandes <jelusarafernandes@gmail.com>
2023-11-06 18:28:47 +0000

    Added client model

---
+++ src/main/java/com/cbfacademy/apiassessment/dao/ClientDao.java
@@ -0,0 +1,13 @@
+package com.cbfacademy.apiassessment.dao;
+
+import com.cbfacademy.apiassessment.model.Client;
+import java.util.UUID;
+
+public interface ClientDao {
+    int insertClient(UUID id, Client client);
+
+    default int addClient(Client client){
+        UUID id = UUID.randomUUID();
+        return insertClient(id, client);
+    }
+}
