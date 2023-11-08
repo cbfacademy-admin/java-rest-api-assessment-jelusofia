6f9ad46f7eb554a0eb2a1a64edbf9527e2d9e655
Jelusara Fernandes <jelusarafernandes@gmail.com>
2023-11-08 18:34:30 +0000

    Added  @requestBody

--- src/main/java/com/cbfacademy/apiassessment/api/ClientController.java
+++ src/main/java/com/cbfacademy/apiassessment/api/ClientController.java
@@ -2,6 +2,7 @@ package com.cbfacademy.apiassessment.api;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cbfacademy.apiassessment.model.Client;
@@ -17,7 +18,7 @@ public class ClientController {
     }
 
     @PostMapping
-    public void addClient(Client client){
+    public void addClient(@RequestBody Client client){
         clientService.addClient(client);
     }
 }
