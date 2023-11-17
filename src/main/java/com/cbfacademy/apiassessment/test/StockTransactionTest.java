a085c2f9c2eaf942b319bfc27c0f6645ea39fb4c
Jelusara Fernandes <jelusarafernandes@gmail.com>
2023-11-17 04:59:28 +0000

    Added JUnit dependencies to pom.xml

--- src/main/java/com/cbfacademy/apiassessment/test/StockTransactionTest.java
+++ src/main/java/com/cbfacademy/apiassessment/test/StockTransactionTest.java
@@ -3,13 +3,20 @@ package com.cbfacademy.apiassessment.test;
 
 import com.cbfacademy.apiassessment.model.StockTransaction;
 
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
-
 import java.util.UUID;
 
 public class StockTransactionTest {
-    
+    @Test
+    public void testCalculateProfit() {
+        // Given
+        StockTransaction transaction = new StockTransaction(UUID.randomUUID(), "ABC", 10, 100.0, 120.0, 5.0, 0.0);
+
+        // When
+        double profit = transaction.calculateProfit();
+
+        // Then
+        assertEquals(950.0, profit, 0.001);
+    }
 
     
 }
