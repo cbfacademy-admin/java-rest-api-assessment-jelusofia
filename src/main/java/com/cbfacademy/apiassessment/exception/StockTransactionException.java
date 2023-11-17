ea65373215fdea7eda089c1b13a657e630148b26
Jelusara Fernandes <jelusarafernandes@gmail.com>
2023-11-17 05:50:44 +0000

    Additional pom.xml changes

--- src/main/java/com/cbfacademy/apiassessment/exception/StockTransactionException.java
+++ src/main/java/com/cbfacademy/apiassessment/exception/StockTransactionException.java
@@ -2,5 +2,17 @@ package com.cbfacademy.apiassessment.exception;
 
 public class StockTransactionException extends RuntimeException {
     
-  //data validation exceptions  
+  //add some data validation exceptions 
+  public StockTransactionException(String message) {
+    super(message);
+  }
+
+  public void ProfitCalculationException(String message) {
+    
+  }
+  public void InvalidTransactionDataException(String message) {
+    
+  }
+
+
 }
--- src/main/java/com/cbfacademy/apiassessment/test/StockTransactionTest.java
+++ src/main/java/com/cbfacademy/apiassessment/test/StockTransactionTest.java
@@ -1,22 +1,8 @@
 package com.cbfacademy.apiassessment.test;
 
-
-import com.cbfacademy.apiassessment.model.StockTransaction;
-
-import java.util.UUID;
-
 public class StockTransactionTest {
-    @Test
-    public void testCalculateProfit() {
-        // Given
-        StockTransaction transaction = new StockTransaction(UUID.randomUUID(), "ABC", 10, 100.0, 120.0, 5.0, 0.0);
 
-        // When
-        double profit = transaction.calculateProfit();
-
-        // Then
-        assertEquals(950.0, profit, 0.001);
-    }
+    
 
     
 }
