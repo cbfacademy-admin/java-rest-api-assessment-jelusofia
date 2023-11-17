8b5051c5296e6a8bc50b1ad5aeeac4e22f1f7d47
Jelusara Fernandes <jelusarafernandes@gmail.com>
2023-11-17 01:26:56 +0000

    Added Exception folder

---
+++ src/main/java/com/cbfacademy/apiassessment/exception/StockTransactionException.java
@@ -0,0 +1,6 @@
+package com.cbfacademy.apiassessment.exception;
+
+public class StockTransactionException extends RuntimeException {
+    
+  //data validation exceptions  
+}
--- src/main/java/com/cbfacademy/apiassessment/model/CustomException.java
+++
@@ -1,6 +0,0 @@
-package com.cbfacademy.apiassessment.model;
-
-public class CustomException extends RuntimeException {
-    
-    
-}
