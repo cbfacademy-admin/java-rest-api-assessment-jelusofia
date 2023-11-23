c19d3f18a645cfee68acb513cc1da2cab537df5b
Jelusara Fernandes <jelusarafernandes@gmail.com>
2023-11-23 01:26:46 +0000

    Chamged to MergeSort

---
+++ src/main/java/com/cbfacademy/apiassessment/algorithm/MergeSortAlgo.java
@@ -0,0 +1,15 @@
+package com.cbfacademy.apiassessment.algorithm;
+
+public class MergeSortAlgo {
+    
+
+    public static void QuickSortAlgo(int[] array, int low, int high){
+        if (low < high) {
+
+        }
+    }
+    public static void main(String[]args){
+
+
+    }
+}
